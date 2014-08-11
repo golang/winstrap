abaa07d688a175aeefb73f11e4aa2c6730f69e9c
Andrew Gerrand <adg@golang.org>
2014-08-11 14:58:52 +1000

    add -release flag for building makerelease tool

--- winstrap.go
+++ winstrap.go
@@ -19,11 +19,17 @@ var files = map[string]string{
 	"ChromeStandaloneSetup.exe": "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BC159FD9F-6827-8E7E-0CC8-7783A1335AA5%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26installdataindex%3Ddefaultbrowser/update2/installers/ChromeStandaloneSetup.exe",
 	"Mercurial.exe":             "http://mercurial.selenic.com/release/windows/Mercurial-3.1-x64.exe",
 	"tdm64-gcc-4.8.1-3.exe":     "http://downloads.sourceforge.net/project/tdm-gcc/TDM-GCC%20Installer/tdm64-gcc-4.8.1-3.exe?r=http%3A%2F%2Ftdm-gcc.tdragon.net%2Fdownload&ts=1407729829&use_mirror=ufpr",
+	wixFilename:                 "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wix&DownloadId=204417&FileTime=129409234222130000&Build=20919",
 }
+
+const wixFilename = "Wix35.msi"
 
 var altMain func()
 
-var flagYes = flag.Bool("yes", false, "Run without prompt")
+var (
+	flagYes = flag.Bool("yes", false, "Run without prompt")
+	release = flag.Bool("release", false, "Set up a release builder")
+)
 
 func main() {
 	if runtime.GOOS != "windows" {
@@ -43,6 +49,9 @@ func main() {
 	log.Printf("Downloading files.")
 	var wg sync.WaitGroup
 	for file, url := range files {
+		if !*release && file == wixFilename {
+			continue
+		}
 		wg.Add(1)
 		go download(file, url, &wg)
 	}
@@ -55,6 +64,10 @@ func main() {
 
 	runGoMakeBat("386")
 	runGoMakeBat("amd64")
+
+	if *release {
+		buildMakerelease()
+	}
 
 	log.Printf(`Installed go to %v, please add %v\bin to your PATH`, goroot(), goroot())
 
@@ -91,6 +104,31 @@ func runGoMakeBat(arch string) {
 		log.Fatalf("make.bat for arch %s: %v", arch, err)
 	}
 	log.Printf("ran make.bat for arch %s", arch)
+}
+
+func buildMakerelease() {
+	goCmd := filepath.Join(goroot(), "bin", "go.exe")
+	mrDir := filepath.Join(goroot(), "misc", "makerelease")
+	env := append(
+		[]string{"GOPATH=" + gopath()},
+		removeEnvs(os.Environ(), "GOPATH")...)
+	bin := filepath.Join(home(), "makerelease.exe")
+
+	goDo := func(args ...string) {
+		cmd := exec.Command(goCmd, args...)
+		cmd.Dir = mrDir
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Env = env
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Print("Fetching dependencies for makerelease...")
+	goDo("get", "-d")
+	log.Println("Building and installing makerelease to", bin)
+	goDo("build", "-o", bin)
 }
 
 func removeEnvs(envs []string, removeKeys ...string) []string {
@@ -180,6 +218,8 @@ func check(err error) {
 func home() string { return os.Getenv("HOMEPATH") }
 
 func goroot() string { return filepath.Join(home(), "goroot") }
+
+func gopath() string { return filepath.Join(home(), "gopath") }
 
 func download(file, url string, wg *sync.WaitGroup) {
 	defer wg.Done()
